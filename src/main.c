fbb17c7a00d19481519002b0b1f1b23c9dbd4363
marlucz <marcin_luczak@hotmail.com>
2024-01-30 22:38:10 +0100

    create cube array orthographic projection

--- src/main.c
+++ src/main.c
@@ -2,8 +2,11 @@
 #include "vector.h"
 
 // declare an array of vectors/points
-const int M_POINTS = 9 * 9 * 0;
-vec3_t cube_points[M_POINTS];  // 9x9x9 cube
+#define N_POINTS (9 * 9 * 9)
+vec3_t cube_points[N_POINTS];       // 9x9x9 cube
+vec2_t projected_points[N_POINTS];  // cube_points projected into 2D space
+
+float fov_factor = 128;
 
 bool is_running = false;
 
@@ -45,15 +48,42 @@ void process_input(void) {
   }
 }
 
-void update(void) {}
+// Function that receives 3D vector and return a projected 2D point
+vec2_t project(vec3_t point) {
+  vec2_t projected_point = {.x = (fov_factor * point.x),
+                            .y = (fov_factor * point.y)};
+
+  return projected_point;
+}
+
+void update(void) {
+  for (int i = 0; i < N_POINTS; i++) {
+    vec3_t point = cube_points[i];
+
+    // project the current point
+    vec2_t projected_point = project(point);
+    // save the projected 2D vector in the array that will be used to render the
+    // points
+    projected_points[i] = projected_point;
+  }
+}
 
 void render(void) {
-  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-  SDL_RenderClear(renderer);
+  // removed because we render color and clear buffer by hand later
+  // SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+  // SDL_RenderClear(renderer);
 
   draw_grid();
-  draw_rect(200, 200, 500, 200, 0xFF0000FF);
-  draw_pixel(20, 20, 0XFFFFFF00);
+  // draw_rect(200, 200, 500, 200, 0xFF0000FF);
+  // draw_pixel(20, 20, 0XFFFFFF00);
+
+  // loop all projected points and render them
+  for (int i = 0; i < N_POINTS; i++) {
+    vec2_t projected_point = projected_points[i];
+    draw_rect(projected_point.x + window_width / 2,
+              projected_point.y + window_height / 2, 4, 4, 0xFFFFFF00);
+  }
+
   render_color_buffer();
   clear_color_buffer(0xFF000000);
 
