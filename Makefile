cb68898c8e6eeefa7cf85595cf2739490b1a2fba
marlucz <marcin_luczak@hotmail.com>
2024-02-08 23:42:19 +0100

    add vector rotation functions

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 build:
-	gcc -Wall -std=c99 -ISDL2/include -LSDL2/lib ./src/*.c -lmingw32 -lSDL2main -lSDL2 -mwindows -o renderer
+	gcc -Wall -std=c99 -ISDL2/include -LSDL2/lib ./src/*.c -lmingw32 -lSDL2main -lSDL2 -lm -mwindows -o renderer
 
 run:
 	./renderer
--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@ vec3_t cube_points[N_POINTS];       // 9x9x9 cube
 vec2_t projected_points[N_POINTS];  // cube_points projected into 2D space
 
 vec3_t camera_position = {.x = 0, .y = 0, .z = -5};
+vec3_t cube_rotation = {.x = 0, .y = 0, .z = 0};
 
 float fov_factor = 640;
 
@@ -59,14 +60,22 @@ vec2_t project(vec3_t point) {
 }
 
 void update(void) {
+  cube_rotation.x += 0.01;
+  cube_rotation.y += 0.01;
+  cube_rotation.z += 0.01;
+
   for (int i = 0; i < N_POINTS; i++) {
     vec3_t point = cube_points[i];
 
-    // move the points away from the camera
-    point.z -= camera_position.z;
+    vec3_t transformed_point = vec3_rotate_x(point, cube_rotation.x);
+    transformed_point = vec3_rotate_y(transformed_point, cube_rotation.y);
+    transformed_point = vec3_rotate_z(transformed_point, cube_rotation.z);
+
+    // translate the points points away from the camera
+    transformed_point.z -= camera_position.z;
 
     // project the current point
-    vec2_t projected_point = project(point);
+    vec2_t projected_point = project(transformed_point);
     // save the projected 2D vector in the array that will be used to render the
     // points
     projected_points[i] = projected_point;
