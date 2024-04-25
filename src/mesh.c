f77f0df3d080331176ff9390e7a86cb7f2ec1ec9
marlucz <marcin_luczak@hotmail.com>
2024-04-25 22:33:58 +0200

    add per face coloring

--- src/mesh.c
+++ src/mesh.c
@@ -1,46 +1,48 @@
 #include "mesh.h"
-#include "array.h"
+
 #include <stdio.h>
 #include <string.h>
+
+#include "array.h"
 
 mesh_t mesh = {
     .vertices = NULL, .faces = NULL, .rotation = {.x = 0, .y = 0, .z = 0}};
 
 vec3_t cube_vertices[N_CUBE_VERTICES] = {
-    {.x = -1, .y = -1, .z = -1}, // 1
-    {.x = -1, .y = 1, .z = -1},  // 2
-    {.x = 1, .y = 1, .z = -1},   // 3
-    {.x = 1, .y = -1, .z = -1},  // 4
-    {.x = 1, .y = 1, .z = 1},    // 5
-    {.x = 1, .y = -1, .z = 1},   // 6
-    {.x = -1, .y = 1, .z = 1},   // 7
-    {.x = -1, .y = -1, .z = 1}   // 8
+    {.x = -1, .y = -1, .z = -1},  // 1
+    {.x = -1, .y = 1, .z = -1},   // 2
+    {.x = 1, .y = 1, .z = -1},    // 3
+    {.x = 1, .y = -1, .z = -1},   // 4
+    {.x = 1, .y = 1, .z = 1},     // 5
+    {.x = 1, .y = -1, .z = 1},    // 6
+    {.x = -1, .y = 1, .z = 1},    // 7
+    {.x = -1, .y = -1, .z = 1}    // 8
 };
 
 face_t cube_faces[N_CUBE_FACES] = {
     // Front
-    {.a = 1, .b = 2, .c = 3},
-    {.a = 1, .b = 3, .c = 4},
+    {.a = 1, .b = 2, .c = 3, .color = 0xFFFF0000},
+    {.a = 1, .b = 3, .c = 4, .color = 0xFFFF0000},
 
     // Right
-    {.a = 4, .b = 3, .c = 5},
-    {.a = 4, .b = 5, .c = 6},
+    {.a = 4, .b = 3, .c = 5, .color = 0xFF00FF00},
+    {.a = 4, .b = 5, .c = 6, .color = 0xFF00FF00},
 
     // Back
-    {.a = 6, .b = 5, .c = 7},
-    {.a = 6, .b = 7, .c = 8},
+    {.a = 6, .b = 5, .c = 7, .color = 0xFF0000FF},
+    {.a = 6, .b = 7, .c = 8, .color = 0xFF0000FF},
 
     // Left
-    {.a = 8, .b = 7, .c = 2},
-    {.a = 8, .b = 2, .c = 1},
+    {.a = 8, .b = 7, .c = 2, .color = 0xFFFF0000},
+    {.a = 8, .b = 2, .c = 1, .color = 0xFFFF0000},
 
     // Top
-    {.a = 2, .b = 7, .c = 5},
-    {.a = 2, .b = 5, .c = 3},
+    {.a = 2, .b = 7, .c = 5, .color = 0xFFFFFF00},
+    {.a = 2, .b = 5, .c = 3, .color = 0xFFFFFF00},
 
     // Bottom
-    {.a = 6, .b = 8, .c = 1},
-    {.a = 6, .b = 1, .c = 4}};
+    {.a = 6, .b = 8, .c = 1, .color = 0xFFFF00FF},
+    {.a = 6, .b = 1, .c = 4, .color = 0xFFFF00FF}};
 
 void load_cube_mesh_data(void) {
   for (int i = 0; i < N_CUBE_VERTICES; i++) {
